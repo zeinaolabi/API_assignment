677b395fba9121f8d064a0f59020d9bb2f58e6a8
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-12 22:50:10 +0300

    Finished Palindrome function

--- palindromeAPI.php
+++ palindromeAPI.php
@@ -1,3 +1,22 @@
 <?php
-
+function Palindrome($string) {
+    $first = 0;
+    $second = strlen($string) - 1;
+    $bool = true;
+  
+    while($first > $second){
+      if ($string[$first] != $string[$second]){
+        $bool = false;
+        break;
+      }
+      $first++;
+      $second--;
+    }
+  
+    if ($bool){
+        return "Palindrome";
+    }else {
+        return "not a Palindrome";
+    }
+  }
 ?>
