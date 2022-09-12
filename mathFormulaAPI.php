a6bf3e4a993e1446a6d042a974bbe717f46fc0e5
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-12 23:50:31 +0300

    Finished second API

---
+++ mathFormulaAPI.php
@@ -0,0 +1,18 @@
+<?php
+$a = $_POST["a"];
+$b = $_POST["b"];
+$c = $_POST["c"];
+
+$results = [
+    "a" => $a,
+    "b" => $b,
+    "c" => $c,
+    "result" => getSolution($a, $b, $c)
+];
+
+echo json_encode($results);
+
+function getSolution($a, $b, $c){
+    return  pow($a,3) + $b * $c - $a / $b;
+}
+?>
